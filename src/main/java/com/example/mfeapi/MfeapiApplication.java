0971391f8ba7841233a7eeb631feec24f3b77474
Luanna Regina Costa <luannareginac@gmail.com>
2024-10-10 11:57:21 -0300

    fist

---
+++ src/main/java/com/example/mfeapi/MfeapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mfeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfeapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mfeapi/MfeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mfeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
